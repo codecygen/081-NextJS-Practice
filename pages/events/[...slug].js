514d1e1cb12c30869a708224c05a06d1f798a761
gingercook <vahitaras@gmail.com>
2022-05-30 21:57:22 -0400

    slug section is worked on

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,39 @@
+import { useRouter } from 'next/router';
+
+import { getFilteredEvents } from '../../dummy-data';
+
 const FilteredEventsPage = () => {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className='center'>Loading...</p>
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  const isInvalidFilter = isNaN(numYear) || isNaN(numMonth) || numYear > 2022
+    || numYear < 2021 || numMonth < 1 || numMonth > 12
+  ;
+
+  if (isInvalidFilter) {
+    return <p className='center'>Invalid filter. Please adjust your values!</p>
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p className='center'>No events found for the chosen filter!</p>
+  }
+
   return (
     <div>FilteredEventsPage</div>
   )
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,21 @@
+import { useRouter } from 'next/router';
+
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/EventList';
 import EventsSearch from '../../components/events/EventsSearch';
 
 const AllEventsPage = () => {
+  const router = useRouter();
   const events = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    const path = `/events/${year}/${month}`;
+    router.push(path);
+  };
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList 
         items={events}
       />
