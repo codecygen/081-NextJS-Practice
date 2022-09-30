70921632cd3507f7ca988b435be0906f3eb979fe
gingercook <vahitaras@gmail.com>
2022-09-29 20:36:27 -0400

    api routes to be created. ENd of day

---
+++ pages/api/newsletter.js
@@ -0,0 +1,9 @@
+const newsletterHandler = (req, res) => {
+    if (req.method === 'POST') {
+        const userEmail = req.body.email;
+
+        if (!user)
+    }
+};
+
+export default newsletterHandler;
