5600329b8237ba8b89a43a0e6daf1ee97efd746e
gingercook <vahitaras@gmail.com>
2022-10-03 17:02:08 -0400

    timeout added to clear notification window

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,10 +1,10 @@
 import { useContext } from 'react';
 
 import classes from './notification.module.css';
-// import NotificationContext from '../../store/notification-context';
+import NotificationContext from '../../store/notification-context';
 
 function Notification(props) {
-    // const notificationCtx = useContext(NotificationContext);
+    const notificationCtx = useContext(NotificationContext);
 
     const { title, message, status } = props;
 
@@ -26,7 +26,7 @@ function Notification(props) {
 
     return (
         <div className={activeClasses}
-            // onClick={notificationCtx.hideNotification}
+            onClick={notificationCtx.hideNotification}
         >
             <h2>{title}</h2>
             <p>{message}</p>
